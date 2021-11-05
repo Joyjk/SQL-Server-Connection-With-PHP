8a29ed5819006f04eccec03712fbd826a7234886
Joy Karmakar <17-35526-3@student.aiub.edu>
2021-11-05 12:18:12 +0600

    SQL Server Connection With PHP update

--- Connection.php
+++ Connection.php
@@ -20,5 +20,12 @@
         die(print_r(sqlsrv_errors(),true));
     }
 
-
+//https://www.youtube.com/watch?v=upvALf8zJXg
+//Link for php7 drivers
+//https://github.com/Microsoft/msphpsql/releases
+//Link for Microsoft ODBC driver for sql server
+//
+//https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbXlCamVDUjhYZHpaci1Yc09fd2c1MzR0MHljQXxBQ3Jtc0tsdXNXSXQ1VVk1c3E2Q1Q2b3lCVUdvZ0NNZktnVGRDVEFlTjU1cDdJWUpSQ3RNenB0RjJ6endHNlZoY0hDSDNxRGswWDFNa0pDYUlmXzcyWXQzNl9PUHdYNTBXbVB3VC1BTkxiZHlWcDlWcTdCS3lYWQ&q=https%3A%2F%2Fwww.microsoft.com%2Fen-in%2Fdownload%2Fconfirmation.aspx%3Fid%3D36434
 ?>
+
+
